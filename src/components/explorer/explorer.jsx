90af35de30d769b9ee37d884b7348a0f927e1646
neil <neil.wu@yahoo.ca>
2026-06-06 10:20:57 -0400

    update tables

--- src/components/explorer/explorer.jsx
+++ src/components/explorer/explorer.jsx
@@ -3,6 +3,7 @@ import ConfirmationDialog from "../ConfirmationDialog";
 import MoveCopyDialog from "../MoveCopyDialog";
 import ServerDownload from "../server_download";
 import UploadDialog from "../UploadDialog";
+import ExplorerTree from "./explorer_tree"
 
 function ExplorerSection(){
     const base_url = "http://localhost:8080/filesystem/"
@@ -265,49 +266,16 @@ function ExplorerSection(){
         <div className="main">
           <ServerDownload isOpen={isDownloadOpen} name={current} remote_url={url} />
           <div className="table-container">
-            <table border="0" cellPadding="10" style={{ borderCollapse: 'collapse', width: '100%' }}>
-              <thead>
-                <tr>
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <div className="table-td-box">Current Directory : {data.parent + "/" + data.name}</div>
-                  </td>
-                  <td></td>
-                </tr>
-                <tr>
-                  <td>
-                    <div className="table-td-box">Parent Directory : <a href="#" name={data.parent} onClick={handleSelection}>{data.parent}</a></div>
-                  </td>
-                  <td></td>
-                </tr>
-                {/* 2. Use .map() to loop through the array and return table rows */}
-                {list.map((item) => (
-                  <tr>
-                    {item.kind === 'folder' && <td><a href="#" name={item.path} onClick={handleSelection}>{item.name}</a></td>}
-                    {item.kind === 'file' && <td>{item.name}</td>}
-                    {item.kind === 'folder' && <td>
-                                                <button name={item.path} parent={item.parent_path} onClick={handleDownload} className="link-button">Download</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleUpload} className="link-button">Upload</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleMove} className="link-button">Move</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleCopy} className="link-button">Copy</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleDelete} className="link-button">Delete</button>
-                                                </td>}
-                    {item.kind === 'file' && <td>
-                                                <button name={item.path} parent={item.parent_path} onClick={handleView} className="link-button">View</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleDownload} className="link-button">Download</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleMove} className="link-button">Move</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleCopy} className="link-button">Copy</button>&nbsp;&nbsp;
-                                                <button name={item.path} parent={item.parent_path} onClick={handleDelete} className="link-button">Delete</button>
-                                                </td>}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
+            <ExplorerTree 
+              data={data}
+              list={list}
+              handleDownload={handleDownload}
+              handleUpload={handleUpload}
+              handleCopy={handleCopy}
+              handleMove={handleMove}
+              handleDelete={handleDelete}
+              handleView={handleView}
+              />
             <ConfirmationDialog 
               isOpen={isDialogOpen} 
               title="Delete a File/Folder" 
---
+++ src/components/explorer/explorer_tree.jsx
@@ -0,0 +1,48 @@
+function ExplorerTree({data,list,handleSelection,handleDownload,handleUpload,handleCopy,handleMove,handleDelete,handleView}){
+  return (
+    <table border="0" cellPadding="10" style={{ borderCollapse: 'collapse', width: '100%' }}>
+      <thead>
+        <tr>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <div className="table-td-box">Current Directory : {data.parent + "/" + data.name}</div>
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>
+            <div className="table-td-box">Parent Directory : <a href="#" name={data.parent} onClick={handleSelection}>{data.parent}</a></div>
+          </td>
+          <td></td>
+        </tr>
+        {/* 2. Use .map() to loop through the array and return table rows */}
+        {list.map((item) => (
+          <tr>
+            {item.kind === 'folder' && <td><a href="#" name={item.path} onClick={handleSelection}>{item.name}</a></td>}
+            {item.kind === 'file' && <td>{item.name}</td>}
+            {item.kind === 'folder' && <td>
+                                        <button name={item.path} parent={item.parent_path} onClick={handleDownload} className="link-button">Download</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleUpload} className="link-button">Upload</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleMove} className="link-button">Move</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleCopy} className="link-button">Copy</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleDelete} className="link-button">Delete</button>
+                                        </td>}
+            {item.kind === 'file' && <td>
+                                        <button name={item.path} parent={item.parent_path} onClick={handleView} className="link-button">View</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleDownload} className="link-button">Download</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleMove} className="link-button">Move</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleCopy} className="link-button">Copy</button>&nbsp;&nbsp;
+                                        <button name={item.path} parent={item.parent_path} onClick={handleDelete} className="link-button">Delete</button>
+                                        </td>}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+export default ExplorerTree;
